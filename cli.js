d02a1e3309cf6d596212c2c725f6ae8e541e2d80
Zeke Sikelianos <zeke@sikelianos.com>
2016-07-25 13:01:03 -0700

    ignore more patterns

--- cli.js
+++ cli.js
@@ -3,30 +3,41 @@
 const globby = require('globby')
 const path = require('path')
 const lintFiles = require('.').lintFiles
-const patterns = [
-  '**/*.md',
-  '**/*.markdown',
+const cwd = process.cwd()
+let patterns = [
+  '!**/.git/**',
+  '!**/coverage/**',
+  '!**/dist/**',
   '!**/node_modules/**',
   '!**/vendor/**',
-  '!**/.git/**'
+  '!*.min.js',
+  '!bundle.js'
 ]
-const cwd = process.argv.slice(2)[0] || process.cwd()
+const userPattern = process.argv.slice(2)[0]
+
+if (userPattern) {
+  patterns.unshift(userPattern)
+} else {
+  patterns.unshift('**/*.md', '**/*.markdown')
+}
+
 const files = globby.sync(patterns, {cwd: cwd})
-  .map(file => path.resolve(cwd,file))
+  .map(file => path.resolve(cwd, file))
 
 lintFiles(files, function (err, results) {
   if (err) throw err
 
+  // No errors
   if (results.every(result => result.errors.length === 0)) {
     process.exit(0)
   }
 
+  // Errors!
   results.forEach(result => {
     result.errors.forEach(error => {
       let filepath = path.relative(cwd, result.file)
       console.log(`${filepath}:${error.line}:${error.column}: ${error.message}`)
     })
   })
-
   process.exit(1)
 })
