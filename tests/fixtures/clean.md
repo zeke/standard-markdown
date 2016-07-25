e349aa11f0530ddb057c859f796285b59393666a
Zeke Sikelianos <zeke@sikelianos.com>
2016-07-24 22:20:31 -0700

    test that clean fixtures don't produce errors

---
+++ tests/fixtures/clean.md
@@ -0,0 +1,14 @@
+# The Clean Readme
+
+This is a markdown file with some javascript code blocks in it.
+
+```js
+console.log('all good here!')
+```
+
+There are no linting errors in this file.
+
+```javascript
+let wibble = 2
+console.log(wibble)
+```
--- tests/index.js
+++ tests/index.js
@@ -3,12 +3,14 @@ const fs = require('fs')
 const path = require('path')
 const standardMarkdown = require('..')
 const dirty = fs.readFileSync(path.join(__dirname, 'fixtures/dirty.md'), 'utf8')
+const clean = fs.readFileSync(path.join(__dirname, 'fixtures/clean.md'), 'utf8')
 
 test('standardMarkdown', function (t) {
   standardMarkdown.lintText(dirty, function (err, results) {
     if (err) throw (err)
     // console.error(JSON.stringify(results, null, 2))
 
+    t.comment('dirty fixture')
     t.equal(results.length, 6, 'returns six linting errors')
 
     t.equal(results[0].message, "'foo' is defined but never used", 'finds errors')
@@ -19,12 +21,17 @@ test('standardMarkdown', function (t) {
     t.equal(results[2].message, 'Extra semicolon.', 'finds errors in second block')
     t.equal(results[2].line, 20, 'identifies correct line number in first block')
 
-    t.comment('every result')
+    t.comment('every error')
     t.ok(results.every(result => result.message.length), 'has a `message` property')
     t.ok(results.every(result => result.line > 0), 'has a `line` property')
     t.ok(results.every(result => result.column > 0), 'has a `column` property')
     t.ok(results.every(result => result.severity > 0), 'has a `severity` property')
 
-    t.end()
+    t.comment('clean fixture')
+    standardMarkdown.lintText(clean, function (err, results) {
+      if (err) throw (err)
+      t.equal(results.length, 0, 'has no errors')
+      t.end()
+    })
   })
 })
