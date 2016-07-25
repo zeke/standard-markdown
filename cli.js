013e295e532142e50c72ef886d59f55c463e6c3b
Zeke Sikelianos <zeke@sikelianos.com>
2016-07-24 22:28:38 -0700

    add CLI

--- cli.js
+++ cli.js
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 
-const glob = require('glob')
-const fs = require('fs')
+const globby = require('globby')
 const path = require('path')
+const lintFiles = require('.').lintFiles
+const patterns = [
+  '**/*.md',
+  '**/*.markdown',
+  '!**/node_modules/**',
+  '!**/vendor/**',
+  '!**/.git/**'
+]
+const cwd = process.argv.slice(2)[0] || process.cwd()
+const files = globby.sync(patterns, {cwd: cwd})
+  .map(file => path.resolve(cwd,file))
+
+lintFiles(files, function (err, results) {
+  if (err) throw err
+
+  if (results.every(result => result.errors.length === 0)) {
+    process.exit(0)
+  }
+
+  results.forEach(result => {
+    result.errors.forEach(error => {
+      console.log(`${result.file}:${error.line}:${error.column}: ${error.message}`)
+    })
+  })
+
+  process.exit(1)
+})
