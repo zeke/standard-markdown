e46f21c122786be23021a1f6f14ed1e405830982
Samuel Attard <samuel.r.attard@gmail.com>
2016-08-09 15:54:33 +1000

    Initial implementation of standard-format

--- cli.js
+++ cli.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 'use strict'
 
+var fs = require('fs')
 var globby = require('globby')
+var mkdirp = require('mkdirp')
 var path = require('path')
-var lintFiles = require('./').lintFiles
+var standardMarkdown = require('./')
 var cwd = process.argv.slice(2)[0] || process.cwd()
+var autoFix = process.argv.slice(3)[0] === '--auto-fix'
+var dryRun = process.argv.slice(4)[0] === '--dry-run'
 var patterns = [
   '**/*.md',
   '**/*.markdown',
@@ -21,20 +25,37 @@ var files = globby.sync(patterns, { cwd: cwd }).map(function (file) {
   return path.resolve(cwd, file)
 })
 
-lintFiles(files, function (err, results) {
-  if (err) throw err
+if (autoFix) {
+  var targetDir = path.resolve(cwd)
+  if (dryRun) targetDir = path.resolve(cwd, '..', path.basename(path.resolve(cwd)) + '_dry_run')
 
-  // No errors
-  if (results.every(function (result) { return result.errors.length === 0 })) {
-    process.exit(0)
-  }
+  var formattedFiles = standardMarkdown.formatFiles(files)
+  console.log(' ')
 
-  // Errors!
-  results.forEach(function (result) {
-    result.errors.forEach(function (error) {
-      var filepath = path.relative(cwd, result.file)
-      console.log(filepath + ':' + error.line + ':' + error.column + ': ' + error.message)
-    })
+  formattedFiles.forEach(function (result) {
+    if (result.originalText !== result.cleanText) {
+      console.log('File has changed: ' + result.file)
+    }
+    var targetFile = result.file.replace(path.resolve(cwd), targetDir)
+    mkdirp.sync(path.dirname(targetFile))
+    fs.writeFileSync(targetFile, result.cleanText)
   })
-  process.exit(1)
-})
+} else {
+  standardMarkdown.lintFiles(files, function (err, results) {
+    if (err) throw err
+
+    // No errors
+    if (results.every(function (result) { return result.errors.length === 0 })) {
+      process.exit(0)
+    }
+
+    // Errors!
+    results.forEach(function (result) {
+      result.errors.forEach(function (error) {
+        var filepath = path.relative(cwd, result.file)
+        console.log(filepath + ':' + error.line + ':' + error.column + ': ' + error.message)
+      })
+    })
+    process.exit(1)
+  })
+}
